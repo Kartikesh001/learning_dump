2c262659ed04e8c9eb3ee26be3bb0a4ccabce864
KARTIKESH SINGH <kartikeshking02@gmail.com>
2023-07-12 18:23:40 +0530

    Add files via upload

---
+++ c++1.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// int sum(int a, int b ){
+//     int c=a+b;
+//     return c;
+// }
+
+
+
+ int main(){
+//     int a, b;
+    // cout<<"enter the first number"<<endl;
+    // cin>>a;
+    // cout<<"enter the second number"<<endl;
+    // cin>>b;
+    // cout<<"the division"<<a/b<<endl;
+//     int age;
+//     cout<<"enter the age";
+//     cin>>age;
+//     switch(expression)
+// {
+//     case 12:
+//    int index =0;
+//    while(index<34)
+//    {
+//     cout<<"we are at index number"<<index<<endl;
+//     index=index+1;
+    // do{
+    //     cout<<"we are at index number"<<index<<endl;
+    //     index=index+1;
+    // }while(index<32);
+    // for (int i =0; i<34; i++);
+    // {
+    //     cout<<"thevalue"<<i<<endl;
+    // }
+    // int add(int a, int b)
+    // {
+    //     int c;
+    //     c = a + b;
+    //     return c;
+    // }
+    // int main(){
+
+    // int a, b;
+    // cout<<"enter the number"<<endl;
+    // cin>>a;
+    // cout<<"enter"<<endl;
+    // cin>>b;
+    // cout<<"the answer"<<add(a,b);
+//     int arr[6];
+//     for( int i = 0; i <6; i++)
+//     {
+//         cout<<"the marks of the students"<<i<<endl;
+//         cin>>arr[i];
+//     }
+
+//    for( int i = 0; i <6; i++)
+//     {
+//         cout<<"the marks of the students"<<i<<arr[i]<<endl;
+//     }
+// int arr2d[2][3] = {{ 1,2,3},{12,3,46}};
+// for (int i=0; i<2;i++)
+// {
+//     for (int j =0; j<3; j++)
+//     {
+//         cout<<"the number is "<<i<<j<<arr2d[1][2]<<endl;
+//     }
+// }
+
+// int a =343;
+// float b = 89.90;
+// cout<<(float)a/34<<endl;
+// cout<<(int)b;
+ 
+// string name = "kartikeh";
+// cout<<name;
+// cout<<name.length()<<endl;
+// cout<<name.substr(0,3)<<endl;
+// cout<<name.substr(0,2);
+// printf("enter the number8");
+// int n;
+// int m;
+// cin>>n;
+// cout<<"enter the number"<<endl;
+// cin>>m;
+// cout<<"answeee"<<n+m<<end
+
+// int num1;
+// int num2;
+// cout<<"enter the first number"<<endl;
+// cin>>num1;
+// cout<<"enter the second number"<<endl;
+// cin>>num2;
+// cout<<"the result"<<sum(num1, num2);
+
+
+// string phrase ="giraffe";
+// cout<<phrase.find('g');
+
+// STRING
+
+// string king ="king1";
+// cout<< king.length();
+// cout<<king.find('a') ;
+// cout<<king[0];
+// cout<<phase.substr(0, 1);
+
+
+// ARRAY
+// int luckynams[]={1, 2, 3};
+// cout<<luckynams[2];
+
+// IF STATEMENTS
+// bool ismale = false;
+// if(ismale){
+//     cout<<"you are a male";
+// }else{
+//     cout<<"tou are not male";
+// 
+
+    return 0;
+} 
+ 
